84a5311c8b70d257e869b698aa774db132e2242a
abhijithb200 <abhijithb007m@gmail.com>
2025-03-27 13:35:24 +0530

    prod ready build

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-tailwind.config = {
-  theme: {
-    extend: {
-      colors: {
-        primary: "#007749",
-        secondary: "#4FB8E8",
-      },
-      borderRadius: {
-        none: "0px",
-        sm: "4px",
-        DEFAULT: "8px",
-        md: "12px",
-        lg: "16px",
-        xl: "20px",
-        "2xl": "24px",
-        "3xl": "32px",
-        full: "9999px",
-        button: "8px",
-      },
-    },
-  },
-};
+
 
 document.addEventListener("DOMContentLoaded", function () {
   // Sticky Header
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./script.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#007749",
+        secondary: "#4FB8E8",
+      },
+      borderRadius: {
+        none: "0px",
+        sm: "4px",
+        DEFAULT: "8px",
+        md: "12px",
+        lg: "16px",
+        xl: "20px",
+        "2xl": "24px",
+        "3xl": "32px",
+        full: "9999px",
+        button: "8px",
+      },
+    },
+  },
+  plugins: [],
+};
